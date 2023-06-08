af668fb98b85880b19e3c813b0a7ba168e0fa3ef
Andrew Mayers <andrew@mayers.us>
2023-06-08 10:51:19 -0400

    Remove the device to friendly model name mapping
    It was out of date, and a PITA to keep up to date.

--- Sources/AnalyticsKit/UserAndDeviceInfo/DeviceInfo.swift
+++ Sources/AnalyticsKit/UserAndDeviceInfo/DeviceInfo.swift
@@ -5,12 +5,11 @@ public struct DeviceInfo {
     public let osVersion: String
     public let ramSizeMB: Int
     public let freeDiskSpaceMB: Int
-    public let device: Platform.Device
+    public let deviceModelIdentifier = Platform.modelIdentifier
 
     public init() {
         self.osVersion = UIDevice.current.systemVersion
         self.ramSizeMB = Int(Platform.deviceRAM.converted(to: .megabytes))
-        self.device = Platform.device
         self.freeDiskSpaceMB = Int((UIDevice.current.freeDiskSpace() ?? 0).converted(to: .megabytes))
     }
 }
